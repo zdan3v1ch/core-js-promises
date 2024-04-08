8da79e683ce6d292bd4bcc4f80946bb7822a7558
Anatoli Zdanevich <anatoli.zdanevitch@gmail.com>
2024-04-08 23:20:54 +0300

    feat: add some resolves

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,13 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  return new Promise((res, rej) => {
+    if (number < 0) {
+      rej();
+    }
+    res(number);
+  });
 }
 
 /**
@@ -33,8 +38,16 @@ function getPromise(/* number */) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return new Promise((res) => {
+    source
+      .then(() => {
+        res('success');
+      })
+      .catch(() => {
+        res('fail');
+      });
+  });
 }
 
 /**
